04f83b655e31cb11a2fbdd0b107b554c1a7db95a
ninfane <marinacrespog@gmail.com>
2021-11-04 13:33:49 -0300

    Ex 2 falta borrado de valor ingresado por teclado

---
+++ Guia 8/ex_2.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <algorithm>
+#include <sstream>
+using namespace std;
+
+int main() {
+	
+	vector<int> v(30);
+	for(int &x: v){
+		x = rand()%26;
+	}
+	
+	for(int x: v){
+		cout << x << " ";
+	}
+	cout << endl;
+	
+	sort(v.begin(),v.end());
+	auto it = v.begin()+10;
+	reverse(it,it+11);
+	
+	cout << "Reversed: " << endl;
+	for(int x: v){
+		cout << x << " ";
+	}
+	cout << endl;
+	
+	vector<int> copia(10);
+	copy(it,it+11,copia.begin());
+	cout << "Copia:" << endl;
+	for(int x: copia){
+		cout << x << " ";
+	}
+	cout << endl;
+	
+	v.erase(it,it+11);
+	cout << "Original modificado: " << endl;
+	for(int x: v){
+		cout << x << " ";
+	}
+	cout << endl;
+	
+	cout << "Ingrese un valor: " << endl;
+	int val; cin >> val;
+	stringstream ss;
+	ss << "Veces que se repite el numero " << val << ": "; 
+	cout << ss.str() << count(v.begin(),v.end(),val);
+	cout << endl;
+	
+	//falta el borrado de todas las repeticiones del valor buscado
+	
+	return 0;
+}
+
